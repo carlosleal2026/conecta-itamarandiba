f0469957555279170ee3bb0fe8af4a2317cd3895
carlosleal2026 <locadoracarlosleal@gmail.com>
2026-09-13 16:17:27 -0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="cabecalho">
+      <div className="logo">CI</div>
+
+      <div>
+        <h1>Conecta Itamarandiba</h1>
+        <p>Sua voz no mapa. Uma cidade em movimento.</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
